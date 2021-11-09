0ff7ef6bd93ceca88f68fda5bcecbbfe8cc8d794
wbjeon2k <wbjeon2k@users.noreply.github.com>
2021-11-09 09:30:17 +0900

    Update HW2.sql

--- HW2.sql
+++ HW2.sql
@@ -9,4 +9,19 @@ SELECT *  FROM new_server;
 --prototypes
 SELECT CONCAT('A',model) AS name, NULL AS feature FROM pc;
 ---PC-->Computer convert
-SELECT CONCAT('A',model) AS name, price, type, cpu, NULL AS feature FROM pc;
+SELECT CONCAT(CONCAT('B',model),code) AS name, price, type, cpu, NULL AS feature FROM pc;
+--SERVER-->Computer
+SELECT CONCAT(CONCAT('B',model),code) AS name, price, 'S' AS type, cpu, NULL AS feature FROM server;
+--desktop --> Computer
+SELECT CONCAT('A',model) AS name, price, 'D' AS type, cpu, NULL AS feature FROM desktop;
+--laptop --> Computer
+SELECT CONCAT('A',model) AS name, price, 'L' AS type, cpu, weight AS feature FROM laptop;
+--Computer --> #1.get all items
+SELECT CONCAT(CONCAT('B',model),code) AS name, price, type, cpu, NULL AS feature FROM pc
+UNION
+SELECT CONCAT(CONCAT('B',model),code) AS name, price, 'S' AS type, cpu, NULL AS feature FROM server
+UNION
+SELECT CONCAT('A',model) AS name, price, 'D' AS type, cpu, NULL AS feature FROM desktop
+UNION
+SELECT CONCAT('A',model) AS name, price, 'L' AS type, cpu, weight AS feature FROM laptop
+ORDER BY type,name;
