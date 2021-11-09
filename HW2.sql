c583c3926251ce1fed13dec3918fa727a5cf8e47
wbjeon2k <wbjeon2k@users.noreply.github.com>
2021-11-09 10:03:05 +0900

    Update HW2.sql

--- HW2.sql
+++ HW2.sql
@@ -25,3 +25,38 @@ SELECT CONCAT('A',model) AS name, price, 'D' AS type, cpu, NULL AS feature FROM 
 UNION
 SELECT CONCAT('A',model) AS name, price, 'L' AS type, cpu, weight AS feature FROM laptop
 ORDER BY type,name;
+--Computer -->#2----
+--A-desktop recommend
+SELECT CONCAT('A',model) AS name, price, 'D' AS type, cpu, NULL AS feature FROM desktop
+WHERE (
+    price <= (SELECT AVG(price) FROM desktop)
+AND
+    cpu >= (SELECT AVG(cpu) FROM desktop)
+);
+--A-laptop recommend
+SELECT CONCAT('A',model) AS name, price, 'L' AS type, cpu, weight AS feature FROM laptop
+WHERE (
+    price <= (SELECT AVG(price) FROM laptop)
+AND
+    cpu >= (SELECT AVG(cpu) FROM laptop)
+);
+--B-PC-desktop recommend
+SELECT CONCAT(CONCAT('B',model),code) AS name, price, type, cpu, NULL AS feature FROM pc
+WHERE(
+    type = 'D' AND
+    price <= (SELECT AVG(price) FROM pc WHERE type='D') AND
+    cpu >= (SELECT AVG(cpu) FROM pc WHERE type='D') 
+);
+--B-PC-laptop recommend
+SELECT CONCAT(CONCAT('B',model),code) AS name, price, type, cpu, NULL AS feature FROM pc
+WHERE(
+    type = 'L' AND
+    price <= (SELECT AVG(price) FROM pc WHERE type='L') AND
+    cpu >= (SELECT AVG(cpu) FROM pc WHERE type='L') 
+);
+--B-server-recommend
+SELECT CONCAT(CONCAT('B',model),code) AS name, price, 'S' AS type, cpu, NULL AS feature FROM server
+WHERE(
+    price <= (SELECT AVG(price) FROM server) AND
+    cpu >= (SELECT AVG(cpu) FROM server) 
+);
